addea2525c76390cff64662cab777ae8a5b41672
urtekin17 <urtekinnuri@gmail.com>
2026-07-15 15:45:08 +0300

    router rollback

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,9 @@ import { createRoot } from 'react-dom/client'
 import "./i18n";
 import './index.css'
 import App from './App.tsx'
-import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
       <App />
-    </HashRouter>
   </StrictMode>
 )
