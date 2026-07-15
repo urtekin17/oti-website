fd00cb88f63d6e2859cc7d3f916b3c4bf29cced1
urtekin17 <urtekinnuri@gmail.com>
2026-07-15 15:15:05 +0300

    update

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -11,7 +11,7 @@ const Contact: React.FC = () => {
         <h1>{t("contact_title")}</h1>
         <p>{t("contact_text")}</p>
         <p>
-          <strong>Email:</strong> urtechsoft@gmail.com
+          <strong>Email: </strong> urtechsoft@gmail.com
         </p>
         <a
           href="https://www.linkedin.com/in/nuri-urtekin-8b31ba152/"
